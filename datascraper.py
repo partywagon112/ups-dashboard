46729243f7e49f167c53c0fd03cb35a65a8521e1
partywagon112 <patrick.curtain.112@outlook.com>
2022-11-02 23:44:27 +1100

    Inital commit

---
+++ datascraper.py
@@ -0,0 +1,9 @@
+import subprocess
+
+# this will need sudo.
+command = ['pwrstat', '-status']
+
+# https://stackoverflow.com/questions/8217613/how-to-get-data-from-command-line-from-within-a-python-program
+p = subprocess.Popen(command)
+text = p.stdout.read()
+retcode = p.wait()
