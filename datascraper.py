cc0faa47ad11b3a0963231e77b797e08778ea11a
partywagon112 <patrick.curtain.112@outlook.com>
2022-11-03 21:40:08 +1100

    added sudo

--- datascraper.py
+++ datascraper.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # this will need sudo.
-command = ['pwrstat', '-status']
+command = ['sudo', 'pwrstat', '-status']
 
 # https://stackoverflow.com/questions/8217613/how-to-get-data-from-command-line-from-within-a-python-program
 p = subprocess.Popen(command)
